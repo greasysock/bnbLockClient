5c20d7dfc24992afb269138a59ee54fea3730861
Christopher Gresock <greasysock@github.com>
2017-06-19 16:42:19 -0400

    add api

--- support/zwayapi.py
+++ support/zwayapi.py
@@ -23,3 +23,4 @@ class Connect():
     def get_code(self, user_id):
         site = self.__url_build('Run/devices[2].instances[0].commandClasses[99].Get({})'.format(user_id))
         r = requests.get(site, auth=self.__auth)
+        print(r.json())
