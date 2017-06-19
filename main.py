1df59d0b35e338a7f2e6034a8707063748b5ea77
Christopher Gresock <greasysock@github.com>
2017-06-19 16:10:36 -0400

    Initial Upload

---
+++ main.py
@@ -0,0 +1,5 @@
+from support import zwayapi
+
+zway = zwayapi.Connect(0)
+
+zway.get_locations()
---
+++ support/zwayapi.py
@@ -0,0 +1,12 @@
+import requests
+
+class Connect():
+    def __init__(self, device_id):
+        self.__device_id = device_id
+        self.__site = 'http://127.0.0.1:8083/ZAutomation/api/v1/'
+    def __url_build(self, url):
+        return self.__site + url
+    def get_locations(self):
+        site = self.__url_build('locations')
+        r = requests.get(site)
+        print(r.json())
