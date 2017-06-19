e285db6d4440190b3e0932228c4981e48601ba80
Christopher Gresock <greasysock@github.com>
2017-06-19 16:30:45 -0400

    add api

--- support/zwayapi.py
+++ support/zwayapi.py
@@ -1,8 +1,13 @@
 import requests
 
+
+
 class Connect():
     def __init__(self, device_id):
         self.__device_id = device_id
+        self.__user = 'admin'
+        self.__pass = '091560Cg'
+        self.__auth = (self.__user, self.__pass)
         self.__site = 'http://127.0.0.1:8083/ZWaveAPI/'
     def __url_build(self, url):
         return self.__site + url
@@ -12,6 +17,6 @@ class Connect():
         print(r.json())
     def put_code(self, user_code):
         site = self.__url_build('Run/devices[2].instances[0].commandClasses[99].Set(1,{},1)'.format(user_code))
-        r = requests.put(site)
+        r = requests.put(site, auth=self.__auth)
         print(r.status_code)
         print(r.json())
